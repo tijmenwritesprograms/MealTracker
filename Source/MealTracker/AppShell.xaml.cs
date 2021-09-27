eb23785ceadeae756e1c3210fac0ea66161d2cef
tijmenwritesprograms <tijmenvanderhilst@gmail.com>
2021-09-27 22:12:55 +0200

    Initial commit: cleaned shell template

---
+++ Source/MealTracker/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MealTracker
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
